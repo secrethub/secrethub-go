06535b91039f071cc839d2d1967a41914a874fef
Simon Barendse <simon.barendse@gmail.com>
2018-12-29 16:08:03 +0100

    Use pointers in fake client
    This is necessary because the fake services might store state.
    This can be used for example to store the parameters a function
    was called with.

--- pkg/secrethub/fakeclient/client.go
+++ pkg/secrethub/fakeclient/client.go
@@ -4,52 +4,52 @@ import "github.com/keylockerbv/secrethub-go/pkg/secrethub"
 
 // Client implements the secrethub.Client interface.
 type Client struct {
-	AccessRuleService AccessRuleService
-	AccountService    AccountService
-	DirService        DirService
-	OrgService        OrgService
-	RepoService       RepoService
-	SecretService     SecretService
-	ServiceService    ServiceService
-	UserService       UserService
+	AccessRuleService *AccessRuleService
+	AccountService    *AccountService
+	DirService        *DirService
+	OrgService        *OrgService
+	RepoService       *RepoService
+	SecretService     *SecretService
+	ServiceService    *ServiceService
+	UserService       *UserService
 }
 
 // AccessRules implements the secrethub.Client interface.
 func (c Client) AccessRules() secrethub.AccessRuleService {
-	return &c.AccessRuleService
+	return c.AccessRuleService
 }
 
 // Accounts implements the secrethub.Client interface.
 func (c Client) Accounts() secrethub.AccountService {
-	return &c.AccountService
+	return c.AccountService
 }
 
 // Dirs implements the secrethub.Client interface.
 func (c Client) Dirs() secrethub.DirService {
-	return &c.DirService
+	return c.DirService
 }
 
 // Orgs implements the secrethub.Client interface.
 func (c Client) Orgs() secrethub.OrgService {
-	return &c.OrgService
+	return c.OrgService
 }
 
 // Repos implements the secrethub.Client interface.
 func (c Client) Repos() secrethub.RepoService {
-	return &c.RepoService
+	return c.RepoService
 }
 
 // Secrets implements the secrethub.Client interface.
 func (c Client) Secrets() secrethub.SecretService {
-	return &c.SecretService
+	return c.SecretService
 }
 
 // Services implements the secrethub.Client interface.
 func (c Client) Services() secrethub.ServiceService {
-	return &c.ServiceService
+	return c.ServiceService
 }
 
 // Users implements the secrethub.Client interface.
 func (c Client) Users() secrethub.UserService {
-	return &c.UserService
+	return c.UserService
 }
