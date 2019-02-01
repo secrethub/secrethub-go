f35a2902dd53348b9d7be118e9ed60aa005cae57
Simon Barendse <simon.barendse@gmail.com>
2019-02-01 15:55:11 +0100

    Remove pointer receivers from org member service

--- pkg/secrethub/org_member.go
+++ pkg/secrethub/org_member.go
@@ -21,26 +21,26 @@ type orgMemberService struct {
 }
 
 // Get retrieves a users organization membership details.
-func (s *orgMemberService) Get(org api.OrgName, username string) (*api.OrgMember, error) {
+func (s orgMemberService) Get(org api.OrgName, username string) (*api.OrgMember, error) {
 	return s.client.GetOrgMember(org.String(), username)
 }
 
 // Invite invites a user to an organization.
-func (s *orgMemberService) Invite(org api.OrgName, username string, role string) (*api.OrgMember, error) {
+func (s orgMemberService) Invite(org api.OrgName, username string, role string) (*api.OrgMember, error) {
 	return s.client.InviteOrg(org.String(), username, role)
 }
 
 // List retrieves all members of the given organization.
-func (s *orgMemberService) List(name api.OrgName) ([]*api.OrgMember, error) {
+func (s orgMemberService) List(name api.OrgName) ([]*api.OrgMember, error) {
 	return s.client.ListOrgMembers(name.String())
 }
 
 // Revoke removes the given user from the organization.
-func (s *orgMemberService) Revoke(name api.OrgName, username string, opts *api.RevokeOpts) (*api.RevokeOrgResponse, error) {
+func (s orgMemberService) Revoke(name api.OrgName, username string, opts *api.RevokeOpts) (*api.RevokeOrgResponse, error) {
 	return s.client.RevokeOrgMember(name.String(), username, opts)
 }
 
 // Update updates the role of a member of the organization.
-func (s *orgMemberService) Update(org api.OrgName, username string, role string) (*api.OrgMember, error) {
+func (s orgMemberService) Update(org api.OrgName, username string, role string) (*api.OrgMember, error) {
 	return s.client.UpdateOrgMember(org.String(), username, role)
 }
