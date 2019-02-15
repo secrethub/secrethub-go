5776e36e1142227a89d396153fb608a409f991a4
Marc Mackenbach <marcmackenbach@gmail.com>
2019-02-15 09:43:49 +0100

    added randchar.Charset
    Added a character set type to help expanding the password generation
    functionality.

--- pkg/randchar/generator.go
+++ pkg/randchar/generator.go
@@ -3,8 +3,6 @@ package randchar
 import (
 	"crypto/rand"
 	"math/big"
-
-	"github.com/keylockerbv/secrethub-go/pkg/errio"
 )
 
 var (
@@ -12,43 +10,104 @@ var (
 	randPatternAlphanumeric = []byte(`0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`)
 	// randPatternSymbols is added to the randPattern when generator.useSymbols is true.
 	randPatternSymbols = []byte(`!@#$%^*-_+=.,?`)
+
+	// Numeric defines a character set containing all numbers.
+	Numeric = Charset("0123456789")
+	// Lowercase defines a character set containing all lowercase letters.
+	Lowercase = Charset("abcdefghijklmnopqrstuvwxyz")
+	// Uppercase defines a character set containing all uppercase letters.
+	Uppercase = Charset("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	// Alphanumeric defines a character set containing letters and numbers.
+	Alphanumeric = Numeric.Add(Lowercase).Add(Uppercase)
+	// Symbols defines a character set containing special characters commonly used for passwords.
+	Symbols = Charset("!@#$%^*-_+=.,?")
+	// All defines a character set containing both alphanumeric and symbol characters.
+	All = Alphanumeric.Add(Symbols)
+	// Similar defines a character set containing similar looking characters.
+	Similar = Charset("ilL1oO0")
 )
 
 // Generator generates random byte arrays.
 type Generator interface {
-	Generate(length int) ([]byte, error)
+	Generate(n int) ([]byte, error)
 }
 
 // NewGenerator creates a new random generator.
 func NewGenerator(useSymbols bool) Generator {
+	charset := Alphanumeric
+	if useSymbols {
+		charset = All
+	}
+
 	return &generator{
-		useSymbols: useSymbols,
+		charset: charset,
 	}
 }
 
+// generator helps generating slices of randomly chosen
+// characters from a given character set.
 type generator struct {
-	useSymbols bool
+	charset Charset
 }
 
-// Generate returns a random byte array of given length.
-func (generator generator) Generate(length int) ([]byte, error) {
-	pattern := randPatternAlphanumeric
-	if generator.useSymbols {
-		pattern = append(pattern, randPatternSymbols...)
-	}
-	return randFromPattern(pattern, length)
+// Generate returns a byte slice of length n filled with randomly chosen characters from the character set.
+func (g generator) Generate(n int) ([]byte, error) {
+	return g.charset.Rand(n)
 }
 
-func randFromPattern(pattern []byte, length int) ([]byte, error) {
-	data := make([]byte, length)
+// Charset is a byte slice with a set of unique characters.
+type Charset []byte
 
-	lengthPattern := len(pattern)
-	for i := 0; i < length; i++ {
-		c, err := rand.Int(rand.Reader, big.NewInt(int64(lengthPattern)))
+// Rand returns a byte slice of length n filled with randomly chosen characters from the set.
+func (cs Charset) Rand(n int) ([]byte, error) {
+	data := make([]byte, n)
+
+	size := big.NewInt(int64(len(cs)))
+	for i := 0; i < n; i++ {
+		randomIndex, err := rand.Int(rand.Reader, size)
 		if err != nil {
-			return nil, errio.Error(err)
+			return nil, err
 		}
-		data[i] = pattern[c.Int64()]
+		data[i] = cs[randomIndex.Int64()]
 	}
 	return data, nil
+}
+
+// Add merges two character sets into one, removing duplicates.
+func (cs Charset) Add(set Charset) Charset {
+	uniques := make(map[byte]struct{})
+	for _, char := range cs {
+		uniques[char] = struct{}{}
+	}
+
+	for _, char := range set {
+		uniques[char] = struct{}{}
+	}
+
+	result := make([]byte, len(uniques))
+	i := 0
+	for char := range uniques {
+		result[i] = char
+		i++
+	}
+
+	return result
+}
+
+// Subtract removes all characters from a set that match a given set of characters.
+func (cs Charset) Subtract(set Charset) Charset {
+	filter := make(map[byte]struct{})
+	for _, char := range set {
+		filter[char] = struct{}{}
+	}
+
+	result := []byte{}
+	for _, char := range cs {
+		_, exists := filter[char]
+		if !exists {
+			result = append(result, char)
+		}
+	}
+
+	return result
 }
