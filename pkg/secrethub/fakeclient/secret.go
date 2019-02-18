d1d7b6bf88331be215f1c3e68977a692b3d601f4
Marc Mackenbach <marcmackenbach@gmail.com>
2019-02-18 13:56:35 +0100

    refactored SecretService path args to string

--- pkg/secrethub/fakeclient/secret.go
+++ pkg/secrethub/fakeclient/secret.go
@@ -18,27 +18,27 @@ type SecretService struct {
 }
 
 // Delete implements the SecretService interface Delete function.
-func (s *SecretService) Delete(path api.SecretPath) error {
+func (s *SecretService) Delete(path string) error {
 	return s.Deleter.Delete(path)
 }
 
 // Exists implements the SecretService interface Exists function.
-func (s *SecretService) Exists(path api.SecretPath) (bool, error) {
+func (s *SecretService) Exists(path string) (bool, error) {
 	return false, nil
 }
 
 // Get implements the SecretService interface Get function.
-func (s *SecretService) Get(path api.SecretPath) (*api.Secret, error) {
+func (s *SecretService) Get(path string) (*api.Secret, error) {
 	return s.Getter.Get(path)
 }
 
 // Write implements the SecretService interface Write function.
-func (s *SecretService) Write(path api.SecretPath, data []byte) (*api.SecretVersion, error) {
+func (s *SecretService) Write(path string, data []byte) (*api.SecretVersion, error) {
 	return s.Writer.Write(path, data)
 }
 
 // ListEvents implements the SecretService interface ListEvents function.
-func (s *SecretService) ListEvents(path api.SecretPath, subjectTypes api.AuditSubjectTypeList) ([]*api.Audit, error) {
+func (s *SecretService) ListEvents(path string, subjectTypes api.AuditSubjectTypeList) ([]*api.Audit, error) {
 	return s.EventLister.ListEvents(path, subjectTypes)
 }
 
@@ -49,39 +49,39 @@ func (s *SecretService) Versions() secrethub.SecretVersionService {
 
 // SecretDeleter mocks the Delete function.
 type SecretDeleter struct {
-	ArgPath api.SecretPath
+	ArgPath string
 	Err     error
 }
 
 // Delete saves the arguments it was called with and returns the mocked response.
-func (d *SecretDeleter) Delete(path api.SecretPath) error {
+func (d *SecretDeleter) Delete(path string) error {
 	d.ArgPath = path
 	return d.Err
 }
 
 // SecretGetter mocks the Get function.
 type SecretGetter struct {
-	ArgPath       api.SecretPath
+	ArgPath       string
 	ReturnsSecret *api.Secret
 	Err           error
 }
 
 // Get saves the arguments it was called with and returns the mocked response.
-func (g *SecretGetter) Get(path api.SecretPath) (*api.Secret, error) {
+func (g *SecretGetter) Get(path string) (*api.Secret, error) {
 	g.ArgPath = path
 	return g.ReturnsSecret, g.Err
 }
 
 // SecretEventLister mocks the ListEvents function.
 type SecretEventLister struct {
-	ArgPath            api.SecretPath
+	ArgPath            string
 	ArgSubjectTypes    api.AuditSubjectTypeList
 	ReturnsAuditEvents []*api.Audit
 	Err                error
 }
 
 // ListEvents saves the arguments it was called with and returns the mocked response.
-func (s *SecretEventLister) ListEvents(path api.SecretPath, subjectTypes api.AuditSubjectTypeList) ([]*api.Audit, error) {
+func (s *SecretEventLister) ListEvents(path string, subjectTypes api.AuditSubjectTypeList) ([]*api.Audit, error) {
 	s.ArgPath = path
 	s.ArgSubjectTypes = subjectTypes
 	return s.ReturnsAuditEvents, s.Err
@@ -89,14 +89,14 @@ func (s *SecretEventLister) ListEvents(path api.SecretPath, subjectTypes api.Aud
 
 // Writer is a wrapper for the arguments and return values of the mocked Writer method.
 type Writer struct {
-	ArgPath        api.SecretPath
+	ArgPath        string
 	ArgData        []byte
 	ReturnsVersion *api.SecretVersion
 	Err            error
 }
 
 // Writer saves the arguments it was called with and returns the mocked response.
-func (w *Writer) Write(path api.SecretPath, data []byte) (*api.SecretVersion, error) {
+func (w *Writer) Write(path string, data []byte) (*api.SecretVersion, error) {
 	w.ArgPath = path
 	w.ArgData = data
 	return w.ReturnsVersion, w.Err
