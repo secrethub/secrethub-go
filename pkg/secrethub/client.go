d84cbcb3a881afe86016d770737a94ad2ee11e96
Marc Mackenbach <marcmackenbach@gmail.com>
2019-02-19 10:30:05 +0100

    moved Client interface to client.go
    This is the place a developer will look first to know how to create
    a client.

--- pkg/secrethub/client.go
+++ pkg/secrethub/client.go
@@ -6,6 +6,70 @@ import (
 	"github.com/keylockerbv/secrethub-go/pkg/errio"
 )
 
+// Client is the SecretHub client.
+type Client interface {
+	AccessRules() AccessRuleService
+	Accounts() AccountService
+	Dirs() DirService
+	Orgs() OrgService
+	Repos() RepoService
+	Secrets() SecretService
+	Services() ServiceService
+	Users() UserService
+}
+
+type clientAdapter struct {
+	client client
+}
+
+// NewClient creates a new SecretHub client.
+// It overrides the default configuration with the options when given.
+func NewClient(credential Credential, opts *ClientOptions) Client {
+	return &clientAdapter{
+		client: newClient(credential, opts),
+	}
+}
+
+// AccessRules returns an AccessRuleService.
+func (c clientAdapter) AccessRules() AccessRuleService {
+	return newAccessRuleService(c.client)
+}
+
+// Accounts returns an AccountService.
+func (c clientAdapter) Accounts() AccountService {
+	return newAccountService(c.client)
+}
+
+// Dirs returns an DirService.
+func (c clientAdapter) Dirs() DirService {
+	return newDirService(c.client)
+}
+
+// Orgs returns an OrgService.
+func (c clientAdapter) Orgs() OrgService {
+	return newOrgService(c.client)
+}
+
+// Repos returns an RepoService.
+func (c clientAdapter) Repos() RepoService {
+	return newRepoService(c.client)
+}
+
+// Secrets returns an SecretService.
+func (c clientAdapter) Secrets() SecretService {
+	return newSecretService(c.client)
+}
+
+// Services returns an ServiceService.
+func (c clientAdapter) Services() ServiceService {
+	return newServiceService(c.client)
+}
+
+// Users returns an UserService.
+func (c clientAdapter) Users() UserService {
+	return newUserService(c.client)
+}
+
 var (
 	errClient = errio.Namespace("client")
 )
