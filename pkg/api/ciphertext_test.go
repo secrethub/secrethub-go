9055ecc7d7852e9c3d2c749b5fcaa14ec3d57c7d
Simon Barendse <simon.barendse@gmail.com>
2019-02-25 10:30:34 +0100

    Fix getValidEncodedCiphertextAES

--- pkg/api/ciphertext_test.go
+++ pkg/api/ciphertext_test.go
@@ -7,7 +7,7 @@ func getValidEncodedCipherTextRSA() crypto.EncodedCiphertextRSA {
 	return "RSA-OAEP$VGh/cyBpcyBhIHRlc3Qgc3RyaW5n$"
 }
 
-// getValidEncodedCipherTextRSA returns a valid EncodedCipherTextRSA to use in tests.
+// getValidEncodedCipherTextAES returns a valid EncodedCipherTextAES to use in tests.
 func getValidEncodedCipherTextAES() crypto.EncodedCiphertextAES {
-	return "AES-GCM$VGh/cyBpcyBhIHRlc3Qgc3RyaW5n$"
+	return "AES-GCM$Lwi6p9ofYSs+FeCHkmt/aacN3A8=$nonce=DeLt3C9ZWZ1I4P+H"
 }
