8c70d3167d90a5c36075ea31e73be0018deb730f
Simon Barendse <simon.barendse@gmail.com>
2019-03-19 09:34:33 +0100

    Rename functions on the me-service to use verbs for actions

--- pkg/secrethub/me.go
+++ pkg/secrethub/me.go
@@ -4,10 +4,10 @@ import "github.com/secrethub/secrethub-go/internals/api"
 
 // MeService handles operations on the authenticated account.
 type MeService interface {
-	// Repos retrieves all repositories of the current user.
-	Repos() ([]*api.Repo, error)
-	// User retrieves the current users details.
-	User() (*api.User, error)
+	// ListRepos retrieves all repositories of the current user.
+	ListRepos() ([]*api.Repo, error)
+	// GetUser retrieves the current users details.
+	GetUser() (*api.User, error)
 }
 
 type meService struct {
@@ -22,12 +22,12 @@ func newMeService(repoService RepoService, userService UserService) MeService {
 	}
 }
 
-// Repos retrieves all repositories of the current user.
-func (ms meService) Repos() ([]*api.Repo, error) {
+// ListRepos retrieves all repositories of the current user.
+func (ms meService) ListRepos() ([]*api.Repo, error) {
 	return ms.repoService.ListMine()
 }
 
-// User retrieves the current users details.
-func (ms meService) User() (*api.User, error) {
+// GetUser retrieves the current users details.
+func (ms meService) GetUser() (*api.User, error) {
 	return ms.userService.Me()
 }
