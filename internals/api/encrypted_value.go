284c016655d93692ef029fdc5b56a2d33b3142b5
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-06-14 14:30:16 +0200

    Add comments

--- internals/api/encrypted_value.go
+++ internals/api/encrypted_value.go
@@ -6,23 +6,29 @@ import (
 	"github.com/secrethub/secrethub-go/internals/crypto"
 )
 
+// Errors
 var (
 	ErrInvalidEncryptionType = errAPI.Code("invalid_encryption_type").Error("invalid encryption method provided for key")
 )
 
+// EncryptionType specifies the encryption method used for an EncryptedValue.
 type EncryptionType string
 
+// Supported values for EncryptionType.
 const (
 	EncryptionTypeRSAAES  EncryptionType = "rsa-oaep-aes-gcm"
 	EncryptionTypeAWSKKMS EncryptionType = "aws-kms"
 )
 
+// EncryptedValue contains a value that is encrypted with a method described by EncryptionType.
+// If the encryption method requires metadata, this is contained in Metadata.
 type EncryptedValue struct {
 	EncryptionType *EncryptionType `json:"encryption_type"`
 	Ciphertext     []byte          `json:"ciphertext"`
 	Metadata       interface{}     `json:"metadata"`
 }
 
+// EncryptionMetadataRSAAES contains all metadata for combined encryption with RSA-OAEP and AES-GCM.
 type EncryptionMetadataRSAAES struct {
 	RSAKeySize      *int   `json:"rsa_key_size"`
 	AESKeySize      *int   `json:"aes_key_size"`
@@ -30,9 +36,11 @@ type EncryptionMetadataRSAAES struct {
 	AESNonce        []byte `json:"aes_nonce"`
 }
 
+// EncryptionMetadataAWSKMS contains all metadata for AWS KMS encryption.
 type EncryptionMetadataAWSKMS struct {
 }
 
+// UnmarshalJSON populates an EncryptedValue from a JSON representation.
 func (ek *EncryptedValue) UnmarshalJSON(b []byte) error {
 	encodedMetadata := json.RawMessage{}
 	ek.Metadata = &encodedMetadata
@@ -61,6 +69,7 @@ func (ek *EncryptedValue) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// ToCiphertextRSAAES converts a EncryptedValue to crypto.CiphertextRSAAES, if the EncryptedValue has the correct type.
 func (ek *EncryptedValue) ToCiphertextRSAAES() (*crypto.CiphertextRSAAES, error) {
 	metadata, ok := ek.Metadata.(*EncryptionMetadataRSAAES)
 	if !ok {
@@ -77,6 +86,7 @@ func (ek *EncryptedValue) ToCiphertextRSAAES() (*crypto.CiphertextRSAAES, error)
 	}, nil
 }
 
+// NewEncryptedValueFromCiphertextRSAAES creates a new EncryptedValue from a crypto.CiphertextRSAAES.
 func NewEncryptedValueFromCiphertextRSAAES(ciphertext crypto.CiphertextRSAAES) *EncryptedValue {
 	encryptionType := EncryptionTypeRSAAES
 
