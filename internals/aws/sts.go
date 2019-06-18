9e5995f049073cb9300092329945a6cc69453711
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-06-18 16:13:06 +0200

    Add AWS Service creater

---
+++ internals/aws/sts.go
@@ -0,0 +1,81 @@
+package aws
+
+import (
+	"bytes"
+
+	"github.com/secrethub/secrethub-go/internals/crypto"
+
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/secrethub/secrethub-go/internals/api"
+)
+
+type ServiceCreator struct {
+	awsSession *session.Session
+	keyID      string
+	role       string
+}
+
+func NewServiceCreator(keyID, role string, cfgs ...*aws.Config) (*ServiceCreator, error) {
+	sess, err := session.NewSession(cfgs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ServiceCreator{
+		awsSession: sess,
+		keyID:      keyID,
+		role:       role,
+	}, nil
+}
+
+func (c ServiceCreator) Type() api.CredentialType {
+	return api.CredentialTypeAWSSTS
+}
+
+func (c ServiceCreator) Verifier() ([]byte, error) {
+	return []byte(c.role), nil
+}
+
+func (c ServiceCreator) AddProof(req *api.CreateCredentialRequest) error {
+	svc := kms.New(c.awsSession)
+
+	plaintext := api.CredentialAWSSTSPlaintextPrefix + c.role
+	encryptReq, _ := svc.EncryptRequest(&kms.EncryptInput{
+		KeyId:     aws.String(c.keyID),
+		Plaintext: []byte(plaintext),
+	})
+
+	err := encryptReq.Sign()
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = encryptReq.HTTPRequest.Write(&buf)
+	if err != nil {
+		return err
+	}
+	req.Proof = &api.CredentialProofAWSSTS{
+		Region:  api.String(svc.SigningRegion),
+		Request: buf.Bytes(),
+	}
+	return nil
+}
+
+func (c ServiceCreator) Wrap(plaintext []byte) (crypto.CiphertextRSAAES, error) {
+	svc := kms.New(c.awsSession)
+
+	_, err := svc.Encrypt(&kms.EncryptInput{
+		Plaintext: plaintext,
+		KeyId:     aws.String(c.keyID),
+	})
+	if err != nil {
+		return crypto.CiphertextRSAAES{}, err
+	}
+	// TODO: return something that actually makes sense.
+	return crypto.CiphertextRSAAES{}, nil
+	//return resp.CiphertextBlob, nil
+}
