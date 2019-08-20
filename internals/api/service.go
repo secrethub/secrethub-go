f4dfb95629c09ebf068575c55cb49a3b8c45010e
Simon Barendse <simon.barendse@gmail.com>
2019-08-20 16:31:28 +0200

    Add type field to api.Service

--- internals/api/service.go
+++ internals/api/service.go
@@ -24,13 +24,23 @@ var (
 	ErrAccessDeniedToKMSKey = errAPI.Code("access_denied").StatusError("access to KMS key is denied", http.StatusForbidden)
 )
 
+// ServiceType is a possible value of Service.Type.
+type ServiceType string
+
+// Service types
+const (
+	ServiceTypeAWS ServiceType = "aws"
+	ServiceTypeRSA ServiceType = "rsa"
+)
+
 // Service represents a service account on SecretHub.
 type Service struct {
-	AccountID   *uuid.UUID `json:"account_id"`
-	ServiceID   string     `json:"service_id"`
-	Repo        *Repo      `json:"repo"`
-	Description string     `json:"description"`
-	CreatedBy   *uuid.UUID `json:"created_by,omitempty"`
+	AccountID   *uuid.UUID  `json:"account_id"`
+	ServiceID   string      `json:"service_id"`
+	Repo        *Repo       `json:"repo"`
+	Description string      `json:"description"`
+	CreatedBy   *uuid.UUID  `json:"created_by,omitempty"`
+	Type        ServiceType `json:"type"`
 }
 
 // Trim removes all non-essential fields from Service for output
