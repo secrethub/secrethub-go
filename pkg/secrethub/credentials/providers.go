a035e0b1fa14162b8b9e5d4459c0d78300aacce0
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-08-27 17:54:54 +0200

    Add missing comments

--- pkg/secrethub/credentials/providers.go
+++ pkg/secrethub/credentials/providers.go
@@ -45,16 +45,21 @@ func UseKey(credentialReader Reader) KeyProvider {
 	}
 }
 
+// KeyProvider is a Provider that reads a key from a Reader.
+// If the key is encrypted with a passphrase, Passphrase() should be called on the KeyProvider to set the Reader that
+// provides the passphrase that can be used to decrypt the key.
 type KeyProvider struct {
 	credentialReader Reader
 	passphraseReader Reader
 }
 
+// Passphrase returns a new Provider that uses the passphraseReader to read a passphrase if the read key is encrypted.
 func (k KeyProvider) Passphrase(passphraseReader Reader) Provider {
 	k.passphraseReader = passphraseReader
 	return k
 }
 
+// Provide implements the Provider interface for a KeyProvider.
 func (k KeyProvider) Provide(httpClient *http.Client) (auth.Authenticator, Decrypter, error) {
 	key, err := ImportKey(k.credentialReader, k.passphraseReader)
 	if err != nil {
