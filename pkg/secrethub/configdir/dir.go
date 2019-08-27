c214dbc19224ff75b6ff05f56ff8b2e8203a9ea0
Marc Mackenbach <marcmackenbach@gmail.com>
2019-08-27 14:27:43 +0200

    Add GoDoc to configdir package

--- pkg/secrethub/configdir/dir.go
+++ pkg/secrethub/configdir/dir.go
@@ -1,3 +1,5 @@
+// Package configdir provides simple functions to manage the SecretHub 
+// configuration directory. 
 package configdir
 
 import (
@@ -10,12 +12,16 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
+// Dir represents the configuration directory located at some path 
+// on the file system.
 type Dir struct {
 	Path string
 }
 
+// Default is the default way to get the location of the SecretHub 
+// configuration directory, sourcing it from the environment variable 
+// SECRETHUB_CONFIG_DIR or falling back to the ~/.secrethub directory.
 func Default() (*Dir, error) {
-	// TODO: move to constant?
 	envDir := os.Getenv("SECRETHUB_CONFIG_DIR")
 	if envDir != "" {
 		return &Dir{
@@ -32,26 +38,31 @@ func Default() (*Dir, error) {
 	}, nil
 }
 
+// Credential returns the file that contains the SecretHub API credential.
 func (c Dir) Credential() *CredentialFile {
 	return &CredentialFile{
 		Path: filepath.Join(c.Path, "credential"),
 	}
 }
 
+// CredentialFile represents the file that contains the SecretHub API credential.
+// By default, it's a file named "credential" in the configuration directory.
 type CredentialFile struct {
 	Path string
 }
 
-func (f *CredentialFile) Write(p []byte) error {
+// Write writes the given bytes to the credential file. 
+func (f *CredentialFile) Write(data []byte) error {
 	// TOOD: correct permission?
 	err := os.MkdirAll(filepath.Dir(f.Path), 0600)
 	if err != nil {
 		return err
 	}
 	// TOOD: correct permission?
-	return ioutil.WriteFile(f.Path, p, 0600)
+	return ioutil.WriteFile(f.Path, data, 0600)
 }
 
+// Exists returns true when a file exists at the path this credential points to. 
 func (f *CredentialFile) Exists() bool {
 	if _, err := os.Stat(f.Path); os.IsNotExist(err) {
 		return false
@@ -59,6 +70,7 @@ func (f *CredentialFile) Exists() bool {
 	return true
 }
 
+// Read reads from the filesystem and returns the contents of the credential file. 
 func (f *CredentialFile) Read() ([]byte, error) {
 	file, err := os.Open(f.Path)
 	if os.IsNotExist(err) {
