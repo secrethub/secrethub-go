2371156ff025cf427f213bfdf07e60f1627e9219
Marton Soos <marton6@users.noreply.github.com>
2019-12-20 11:14:41 +0100

    Add org iterator

--- pkg/secrethub/org.go
+++ pkg/secrethub/org.go
@@ -3,6 +3,7 @@ package secrethub
 import (
 	"github.com/secrethub/secrethub-go/internals/api"
 	"github.com/secrethub/secrethub-go/internals/errio"
+	"github.com/secrethub/secrethub-go/pkg/secrethub/iterator"
 )
 
 // OrgService handles operations on organisations on SecretHub.
@@ -17,6 +18,8 @@ type OrgService interface {
 	Delete(name string) error
 	// ListMine returns the organizations of the current user.
 	ListMine() ([]*api.Org, error)
+	// Iterator returns an iterator that lists all organizations of the current users.
+	Iterator() OrgIterator
 }
 
 func newOrgService(client *Client) OrgService {
@@ -72,4 +75,42 @@ func (s orgService) Members() OrgMemberService {
 // ListMine returns the organizations of the current user.
 func (s orgService) ListMine() ([]*api.Org, error) {
 	return s.client.httpClient.ListMyOrgs()
+}
+
+// Iterator returns an iterator that lists all organizations of the current users.
+func (s orgService) Iterator() OrgIterator {
+	data, err := s.ListMine()
+	return &orgIterator{
+		index: 0,
+		data:  data,
+		err:   err,
+	}
+}
+
+// RepoIteratorParams defines parameters used when listing repos.
+type OrgIteratorParams struct{}
+
+// RepoIterator iterates over repositories.
+type OrgIterator interface {
+	Next() (api.Org, error)
+}
+
+type orgIterator struct {
+	index int
+	data  []*api.Org
+	err   error
+}
+
+// Next returns the next repo or iterator.Done as an error if there are no more repos.
+func (it *orgIterator) Next() (api.Org, error) {
+	if it.err != nil {
+		return api.Org{}, it.err
+	}
+	if it.index >= len(it.data) {
+		return api.Org{}, iterator.Done
+	}
+
+	element := *it.data[it.index]
+	it.index++
+	return element, nil
 }
