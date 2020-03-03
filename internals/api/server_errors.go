ae32fec19de0f8bf1b5196347485070d333466cf
Marton Soos <marton.soos@secrethub.io>
2020-03-03 10:57:40 +0100

    Update api.IsErrNotFound to check for secret not found errors

--- internals/api/server_errors.go
+++ internals/api/server_errors.go
@@ -104,9 +104,14 @@ var (
 
 // IsErrNotFound returns whether the given error is caused by a un-existing resource.
 func IsErrNotFound(err error) bool {
-	statusError, ok := err.(errio.PublicStatusError)
+	publicError, ok := err.(errio.PublicError)
+	if ok {
+		return publicError.Namespace == errio.Namespace("client") && publicError.Code == "secret_not_found"
+	}
+
+	publicStatusError, ok := err.(errio.PublicStatusError)
 	if !ok {
 		return false
 	}
-	return statusError.StatusCode == 404
+	return publicStatusError.StatusCode == 404
 }
