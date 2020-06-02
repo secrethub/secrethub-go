56585516ac6ff2b55c2694a9345103627f603a8f
Joris Coenen <jpcoenen@users.noreply.github.com>
2020-06-02 18:30:14 +0200

    Test gcp package

---
+++ internals/gcp/kms_decrypter_test.go
@@ -0,0 +1,62 @@
+package gcp
+
+import (
+	"errors"
+	"testing"
+
+	"google.golang.org/api/cloudkms/v1"
+
+	"github.com/secrethub/secrethub-go/internals/assert"
+
+	"github.com/secrethub/secrethub-go/internals/api"
+)
+
+var errTest = errors.New("test-error")
+
+func TestGCPDecrypter_Unwrap(t *testing.T) {
+	defaultCiphertext := []byte("ciphertext")
+	defaultKMSKey := "projects/secrethub-test-1234567890.iam/locations/global/keyRings/test/cryptoKeys/test"
+	defaultPlaintext := "plaintext"
+
+	cases := map[string]struct {
+		input       *api.EncryptedData
+		plaintext   string
+		decryptErr  error
+		expected    []byte
+		expectedErr error
+	}{
+		"success": {
+			input:     api.NewEncryptedDatGCPKMS(defaultCiphertext, api.NewEncryptionKeyGCP(defaultKMSKey)),
+			plaintext: defaultPlaintext,
+			expected:  []byte(defaultPlaintext),
+		},
+		"invalid EncryptedData": {
+			input:       api.NewEncryptedDataAESGCM(defaultCiphertext, []byte("nonce"), 10, api.NewEncryptionKeyLocal(256)),
+			expectedErr: api.ErrInvalidKeyType,
+		},
+		"decryption error": {
+			input:       api.NewEncryptedDatGCPKMS(defaultCiphertext, api.NewEncryptionKeyGCP(defaultKMSKey)),
+			decryptErr:  errTest,
+			expectedErr: errTest,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			decrypter := KMSDecrypter{
+				decryptFunc: func(name string, ciphertext string) (*cloudkms.DecryptResponse, error) {
+					assert.Equal(t, ciphertext, string(defaultCiphertext))
+					return &cloudkms.DecryptResponse{
+						Plaintext: tc.plaintext,
+					}, tc.decryptErr
+				},
+			}
+
+			res, err := decrypter.Unwrap(tc.input)
+			assert.Equal(t, err, tc.expectedErr)
+			if tc.expectedErr == nil {
+				assert.Equal(t, res, tc.expected)
+			}
+		})
+	}
+}
---
+++ internals/gcp/service_creator_test.go
@@ -0,0 +1,56 @@
+package gcp
+
+import (
+	"testing"
+
+	"google.golang.org/api/cloudkms/v1"
+
+	"github.com/secrethub/secrethub-go/internals/api"
+
+	"github.com/secrethub/secrethub-go/internals/assert"
+)
+
+func TestServiceCreator_Wrap(t *testing.T) {
+	kmsKeyID := "123456"
+	ciphertext := []byte("ciphertext")
+
+	cases := map[string]struct {
+		encryptErr error
+
+		expectedErr error
+		expected    *api.EncryptedData
+	}{
+		"success": {
+			expected: api.NewEncryptedDatGCPKMS(ciphertext, api.NewEncryptionKeyGCP(kmsKeyID)),
+		},
+		"encrypt error": {
+			encryptErr:  errTest,
+			expectedErr: errTest,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			plaintext := []byte("plaintext")
+
+			sc := CredentialCreator{
+				encryptFunc: func(name string, pt string) (*cloudkms.EncryptResponse, error) {
+					assert.Equal(t, name, kmsKeyID)
+					assert.Equal(t, pt, string(plaintext))
+					return &cloudkms.EncryptResponse{
+						Ciphertext: string(ciphertext),
+					}, tc.encryptErr
+				},
+			}
+			sc.keyResourceID = kmsKeyID
+
+			res, err := sc.Wrap(plaintext)
+			assert.Equal(t, err, tc.expectedErr)
+
+			if tc.expectedErr == nil {
+				assert.Equal(t, res, tc.expected)
+			}
+
+		})
+	}
+}
