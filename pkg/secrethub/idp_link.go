d465926a4a6af0bf44cae857a817b7ed86586a66
Simon Barendse <simon.barendse@gmail.com>
2020-07-02 11:44:00 +0200

    Return an iterator when listing GCP identity provider links

--- pkg/secrethub/idp_link.go
+++ pkg/secrethub/idp_link.go
@@ -3,6 +3,7 @@ package secrethub
 import (
 	"github.com/secrethub/secrethub-go/internals/api"
 	"github.com/secrethub/secrethub-go/internals/oauthorizer"
+	"github.com/secrethub/secrethub-go/pkg/secrethub/iterator"
 )
 
 type IDPLinkService interface {
@@ -25,7 +26,7 @@ func (i idpLinkService) GCP() IDPLinkGCPService {
 
 type IDPLinkGCPService interface {
 	Create(namespace string, projectID string, authorizationCode string, redirectURI string) (*api.IdentityProviderLink, error)
-	List(namespace string) ([]*api.IdentityProviderLink, error)
+	List(namespace string, params *IdpLinkIteratorParams) IdpLinkIterator
 	Get(namespace string, projectID string) (*api.IdentityProviderLink, error)
 	Exists(namespace string, projectID string) (bool, error)
 	Delete(namespace string, projectID string) error
@@ -49,8 +50,39 @@ func (i idpLinkGCPService) Create(namespace string, projectID string, authorizat
 	})
 }
 
-func (i idpLinkGCPService) List(namespace string) ([]*api.IdentityProviderLink, error) {
-	return i.client.httpClient.ListIDPLinks(namespace, api.IdentityProviderLinkGCP)
+// List returns an iterator that retrieves all GCP identity provider links for the given namespace.
+//
+// Usage:
+//  iter := client.IDPLinks().GCP().List(namespace, &secrethub.IdpLinkIteratorParams{})
+//  for {
+//  	idpLink, err := iter.Next()
+//  	if err == iterator.Done {
+//  		break
+//  	} else if err != nil {
+//  		// Handle error
+//  	}
+//
+//  	// Use identity provider link
+//  }
+func (i idpLinkGCPService) List(namespace string, params *IdpLinkIteratorParams) IdpLinkIterator {
+	return &idpLinkIterator{
+		iterator: iterator.New(
+			iterator.PaginatorFactory(
+				func() ([]interface{}, error) {
+					idpLinks, err := i.client.httpClient.ListIDPLinks(namespace, api.IdentityProviderLinkGCP)
+					if err != nil {
+						return nil, err
+					}
+
+					res := make([]interface{}, len(idpLinks))
+					for i, idpLink := range idpLinks {
+						res[i] = idpLink
+					}
+					return res, nil
+				},
+			),
+		),
+	}
 }
 
 func (i idpLinkGCPService) Get(namespace string, projectID string) (*api.IdentityProviderLink, error) {
@@ -87,4 +119,26 @@ func (i idpLinkGCPService) AuthorizationCodeListener(namespace string, projectID
 
 	authorizer := oauthorizer.NewAuthorizer(oauthConfig.AuthURI, oauthConfig.ClientID, oauthConfig.Scopes...)
 	return oauthorizer.NewCallbackHandler(redirectURL, authorizer)
+}
+
+// IdpLinkIteratorParams defines parameters used when listing identity provider links.
+type IdpLinkIteratorParams struct{}
+
+// IdpLinkIterator iterates over identity provider links.
+type IdpLinkIterator interface {
+	Next() (api.IdentityProviderLink, error)
+}
+
+type idpLinkIterator struct {
+	iterator iterator.Iterator
+}
+
+// Next returns the next identity provider link or iterator.Done as an error if all of them have been returned.
+func (it *idpLinkIterator) Next() (api.IdentityProviderLink, error) {
+	item, err := it.iterator.Next()
+	if err != nil {
+		return api.IdentityProviderLink{}, err
+	}
+
+	return *item.(*api.IdentityProviderLink), nil
 }
