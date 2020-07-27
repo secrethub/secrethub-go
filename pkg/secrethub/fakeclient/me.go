116d4886dcc3d6eecf15d679af0f8d6bfcf5d422
Horia Culea <horiaculea@gmail.com>
2020-07-27 12:20:49 +0300

    MeClient mock

--- pkg/secrethub/fakeclient/me.go
+++ pkg/secrethub/fakeclient/me.go
@@ -1,5 +1,30 @@
 package fakeclient
 
-type MeService struct {
+import (
+	"github.com/secrethub/secrethub-go/internals/api"
+	"github.com/secrethub/secrethub-go/pkg/secrethub"
+)
 
+type MeService struct {
+	GetUserFunc func() (*api.User, error)
+	SendVerificationEmailFunc func() error
+	ListReposFunc func() ([]*api.Repo, error)
+	RepoIteratorFunc func(_ *secrethub.RepoIteratorParams) secrethub.RepoIterator
+	secrethub.MeService
+}
+
+func (m *MeService) GetUser() (*api.User, error) {
+	return m.GetUserFunc()
+}
+
+func (m *MeService) SendVerificationEmail() error {
+	return m.SendVerificationEmailFunc()
+}
+
+func (m *MeService) ListRepos() ([]*api.Repo, error) {
+	return m.ListReposFunc()
+}
+
+func (m *MeService) RepoIterator(repoIteratorParams *secrethub.RepoIteratorParams) secrethub.RepoIterator {
+	return m.RepoIteratorFunc(repoIteratorParams)
 }
