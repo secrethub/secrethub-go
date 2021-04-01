254bb277f0443d0f8a029c6d4d9d09e44c154681
Simon Barendse <simon.barendse@gmail.com>
2021-04-01 19:20:00 +0200

    Fix: don't continue test on error
    Just printing the error isn't sufficient. In case of an error in
    these situations, we shouldn't continue, as it results in a
    nil-pointer derference.

--- pkg/secrethub/acl_test.go
+++ pkg/secrethub/acl_test.go
@@ -14,20 +14,14 @@ import (
 
 func TestReencypter_reencrypt(t *testing.T) {
 	fromPrivateKey, err := crypto.GenerateRSAPrivateKey(2048)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
+	assert.OK(t, err)
 
 	fromPublicKey := fromPrivateKey.Public()
 	fromEncodedPublicKey, err := fromPublicKey.Encode()
-	if err != nil {
-		fmt.Print(err.Error())
-	}
+	assert.OK(t, err)
 
 	forPrivateKey, err := crypto.GenerateRSAPrivateKey(2048)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
+	assert.OK(t, err)
 
 	forPublicKey := forPrivateKey.Public()
 	forEncodedPublicKey, err := forPublicKey.Encode()
@@ -108,7 +102,7 @@ func TestReencypter_reencrypt(t *testing.T) {
 			client:     &fakeClient,
 		}
 
-		encryptedTree := createEncryptedTree(tc.dirs, tc.secrets, fakeReencrypter, firstAccount)
+		encryptedTree := createEncryptedTree(t, tc.dirs, tc.secrets, fakeReencrypter, firstAccount)
 		err = fakeReencrypter.reencrypt(&encryptedTree, &fromPrivateKey)
 		if err != nil {
 			fmt.Print(err.Error())
@@ -118,9 +112,7 @@ func TestReencypter_reencrypt(t *testing.T) {
 		for _, dirTemp := range fakeReencrypter.dirs {
 			assert.Equal(t, dirTemp.AccountID, secondAccount.AccountID)
 			decryptedDir, err := encryptedTree.Directories[tc.dirs[count].DirID].Decrypt(&fromPrivateKey)
-			if err != nil {
-				fmt.Print(err.Error())
-			}
+			assert.OK(t, err)
 			assert.Equal(t, tc.dirs[count].Name, decryptedDir.Name)
 			count++
 		}
@@ -130,25 +122,21 @@ func TestReencypter_reencrypt(t *testing.T) {
 		for _, secretTemp := range fakeReencrypter.secrets {
 			assert.Equal(t, secretTemp.Name.AccountID, secondAccount.AccountID)
 			decryptedSecret, err := encryptedTree.Secrets[count].Decrypt(&fromPrivateKey)
-			if err != nil {
-				fmt.Print(err.Error())
-			}
+			assert.OK(t, err)
 			assert.Equal(t, tc.secrets[count].Name, decryptedSecret.Name)
 			count++
 		}
 	}
 }
 
-func createEncryptedTree(dirs []*api.Dir, secrets []*api.Secret, reencrypter reencrypter, account api.Account) api.EncryptedTree {
+func createEncryptedTree(t *testing.T, dirs []*api.Dir, secrets []*api.Secret, reencrypter reencrypter, account api.Account) api.EncryptedTree {
 	encryptedTree := api.EncryptedTree{
 		Directories: make(map[uuid.UUID]*api.EncryptedDir),
 		Secrets:     make([]*api.EncryptedSecret, len(secrets)),
 	}
 	for _, dir := range dirs {
 		request, err := reencrypter.client.encryptDirFor(dir, &account)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
+		assert.OK(t, err)
 		encryptedTree.Directories[dir.DirID] = &api.EncryptedDir{
 			DirID:         dir.DirID,
 			EncryptedName: request.EncryptedName,
@@ -157,9 +145,7 @@ func createEncryptedTree(dirs []*api.Dir, secrets []*api.Secret, reencrypter ree
 
 	for i, secret := range secrets {
 		request, err := reencrypter.client.encryptSecretFor(secret, &account)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
+		assert.OK(t, err)
 		encryptedTree.Secrets[i] = &api.EncryptedSecret{
 			DirID:         secret.DirID,
 			EncryptedName: request.Name.EncryptedName,
